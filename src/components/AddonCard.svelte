0b0feba65e3880331c1e14f62ad62cbb397cb6ca
Terence Martin <github@nurdz.com>
2022-07-15 23:42:36 -0700

    Update the addon service and component card
    We want to make sure that the addons are returned in the order in which
    they were created, and for that we can search based on the ksuid, which
    encodes the timestamp of the creation of the key id.
    
    In addition to that, this adds in the missing badge display for whether
    a component has chat integrations, overlay integrations, or both.

--- src/components/AddonCard.svelte
+++ src/components/AddonCard.svelte
@@ -7,7 +7,14 @@
 <div class="card lg:card-side bg-base-100 shadow-2xl shadow-neutral text-base-content mb-8">
   <figure><img class="w-28 h-28 mx-4" src={addon.iconPic} alt="Addon Icon"></figure>
   <div class="card-body">
-    <a href="/addons/{addon.slug}"><h2 class="card-title">{addon.title}</h2></a>
+    <h2 class="card-title"><a href="/addons/{addon.slug}">{addon.name}</a>
+      {#if addon.requiresChat}
+        <span class="badge">Chat</span>
+      {/if}
+      {#if addon.requiresOverlay}
+        <span class="badge">Overlay</span>
+      {/if}
+    </h2>
     <MarkdownBox source={addon.description} />
     <div class="card-actions justify-end">
       <button class="btn btn-primary">Add to Channel</button>
--- src/routes/api/v1/addons/index.js
+++ src/routes/api/v1/addons/index.js
@@ -1,4 +1,5 @@
 import { db } from '$lib/db';
+import ksuid from 'ksuid';
 
 
 // =============================================================================
@@ -6,9 +7,16 @@ import { db } from '$lib/db';
 
 /* Obtain the complete list of addons that are known to the system. */
 export async function GET({ url }) {
+  const result = await db.twitchAddon.findMany({});
+
+  // Parse the timestamps out of the entry ID's, and then sort based on them to
+  // put the entries into their creation order.
+  result.forEach(entry => entry.timestamp = ksuid.parse(entry.addonId).timestamp);
+  result.sort((a, b) => a.timestamp - b.timestamp);
+
   return {
     status: 200,
-    body: await db.twitchAddon.findMany({})
+    body: result
   }
 }
 
