9df10fecf3eb3cf583653ce90987af7ed3a422e0
Terence Martin <github@nurdz.com>
2022-07-15 22:56:34 -0700

    Include NavBar tooltips and change profile style
    As a light change, this includes tooltips for when you hover over the
    main navigation bar images, to better show what they're for.
    
    In addition, the profile button is also modified to not include the ring
    since it's a little visually jarring.

--- src/components/layout/navbar/ProfileBtn.svelte
+++ src/components/layout/navbar/ProfileBtn.svelte
@@ -2,20 +2,23 @@
   import { session } from '$app/stores';
   import { goto } from '$app/navigation';
 
+  const tooltip = ($session.user === undefined ? 'Login with Twitch' : 'View Profile')
   const openProfile = () => goto($session.user === undefined ? '/login' : '/profile');
 </script>
 
-<button class="btn btn-circle btn-ghost" on:click={openProfile}>
-  <div class="avatar">
-    <div class="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-1">
-      {#if $session.user !== undefined}
-        <img src="{$session.user.profilePic}" class="object-cover w-full h-full" alt="avatar" title="{$session.user.displayName}">
-      {:else}
-        <svg class="w-full h-full fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8ZM12 11C13.6569 11 15 9.65685 15 8C15 6.34315 13.6569 5 12 5C10.3431 5 9 6.34315 9 8C9 9.65685 10.3431 11 12 11Z"></path>
-          <path d="M6.34315 16.3431C4.84285 17.8434 4 19.8783 4 22H6C6 20.4087 6.63214 18.8826 7.75736 17.7574C8.88258 16.6321 10.4087 16 12 16C13.5913 16 15.1174 16.6321 16.2426 17.7574C17.3679 18.8826 18 20.4087 18 22H20C20 19.8783 19.1571 17.8434 17.6569 16.3431C16.1566 14.8429 14.1217 14 12 14C9.87827 14 7.84344 14.8429 6.34315 16.3431Z"></path>
-        </svg>
-      {/if}
+<div class="tooltip tooltip-bottom tooltip-left" data-tip={tooltip}>
+  <button class="btn btn-circle btn-ghost" on:click={openProfile}>
+    <div class="avatar">
+      <div class="w-8 rounded-full">
+        {#if $session.user !== undefined}
+          <img src="{$session.user.profilePic}" class="object-cover w-full h-full" alt="avatar">
+        {:else}
+          <svg class="w-full h-full fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8ZM12 11C13.6569 11 15 9.65685 15 8C15 6.34315 13.6569 5 12 5C10.3431 5 9 6.34315 9 8C9 9.65685 10.3431 11 12 11Z"></path>
+            <path d="M6.34315 16.3431C4.84285 17.8434 4 19.8783 4 22H6C6 20.4087 6.63214 18.8826 7.75736 17.7574C8.88258 16.6321 10.4087 16 12 16C13.5913 16 15.1174 16.6321 16.2426 17.7574C17.3679 18.8826 18 20.4087 18 22H20C20 19.8783 19.1571 17.8434 17.6569 16.3431C16.1566 14.8429 14.1217 14 12 14C9.87827 14 7.84344 14.8429 6.34315 16.3431Z"></path>
+          </svg>
+        {/if}
+      </div>
     </div>
-  </div>
-</button>
+  </button>
+</div>
--- src/components/layout/navbar/ThemeBtn.svelte
+++ src/components/layout/navbar/ThemeBtn.svelte
@@ -10,6 +10,8 @@
   }
 </script>
 
-<button on:click={toggleDarkMode} class="btn btn-circle">
-  <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
-</button>
+<div class="tooltip tooltip-bottom" data-tip="Toggle Theme">
+  <button on:click={toggleDarkMode} class="btn btn-circle">
+    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+  </button>
+</div>
