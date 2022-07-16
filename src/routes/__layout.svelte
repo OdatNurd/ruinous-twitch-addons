ee4927df7633231f364ea9d4de74da7c65fcbee3
Terence Martin <github@nurdz.com>
2022-07-15 21:15:57 -0700

    Implement a new page footer using DaisyUI
    This is similar to the previous one, but uses a poop icon instead of
    text like the old one did, and also includes a link to the YouTube
    channel as well.
    
    As a part of this change, the layout of the page in general was changed
    to not be padded on all edges.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,16 +3,6 @@
   import { NavBar, Footer } from '$components';
 </script>
 
-
-<div class="p-8 max-w-8xl mx-auto">
-  <NavBar />
-  <slot></slot>
-  <Footer />
-</div>
-
-
-<!-- <div class="flex flex-col p-4 max-w-8xl mx-auto h-screen justify-between">
-  <NavBar />
-  <div class="mb-auto"><slot></slot></div>
-  <Footer />
-</div> -->
+<NavBar />
+<slot></slot>
+<Footer />
